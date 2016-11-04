771057fab60f0ed3b84b998be8da8fae72d604f7
sowrd299 <ryan.ward46@gmail.com>
2016-11-04 13:09:28 -0700

    Adds c execitible

---
+++ cttm.sh
@@ -0,0 +1,10 @@
+#the commands to run the game
+#for 'nix systems
+pushd ./src
+clear
+../bin/cttm.o
+popd
+
+#LUA VERSION
+#cd ./src
+#lua main.lua
---
+++ make.sh
@@ -0,0 +1,1 @@
+cc -o bin/cttm.o src/c/launcher.c -I/usr/include/lua5.2 -llua5.2
